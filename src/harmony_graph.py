a25fe2aba0741f12753175d46784680014bdd8c5
adammatthes <adammatthes@gmail.com>
2025-07-25 13:59:33 -0400

    Update harmony graph and extend test

--- src/harmony_graph.py
+++ src/harmony_graph.py
@@ -2,21 +2,43 @@ from romannumeral import *
 
 
 I = Roman_Numeral(Numeral.I, Inversion.ROOT, Mode.MAJOR)
+I6 = Roman_Numeral(Numeral.I, Inversion.FIRST_INVERSION, Mode.MAJOR)
+
 ii = Roman_Numeral(Numeral.ii, Inversion.ROOT, Mode.MINOR)
+ii6 = Roman_Numeral(Numeral.ii, Inversion.FIRST_INVERSION, Mode.MINOR)
+
 iii = Roman_Numeral(Numeral.iii, Inversion.ROOT, Mode.MINOR)
+iii6 = Roman_Numeral(Numeral.iii, Inversion.FIRST_INVERSION, Mode.MINOR)
+
 IV = Roman_Numeral(Numeral.IV, Inversion.ROOT, Mode.MAJOR)
+IV6 = Roman_Numeral(Numeral.IV, Inversion.FIRST_INVERSION, Mode.MAJOR)
+
 V = Roman_Numeral(Numeral.V, Inversion.ROOT, Mode.MAJOR)
+V6 = Roman_Numeral(Numeral.V, Inversion.FIRST_INVERSION, Mode.MAJOR)
+V64 = Roman_Numeral(Numeral.V, Inversion.SECOND_INVERSION, Mode.MAJOR)
+
 vi = Roman_Numeral(Numeral.vi, Inversion.ROOT, Mode.MINOR)
-vii = Roman_Numeral(Numeral.vii, Inversion.ROOT, Mode.DIMINISHED)
+vi6 = Roman_Numeral(Numeral.vi, Inversion.FIRST_INVERSION, Mode.MINOR)
+
+viio = Roman_Numeral(Numeral.vii, Inversion.ROOT, Mode.DIMINISHED)
+viio6 = Roman_Numeral(Numeral.vii, Inversion.FIRST_INVERSION, Mode.DIMINISHED)
 
 class Harmony_Graph():
     def __init__(self):
         self.graph = {
-            I: {iii, IV, V, vi, vii},
-            ii: {IV, V, vi, vii},
-            iii: {V, vi, vii},
-            IV: {V, vi, vii, I},
-            V: {I, vi, vii},
-            vi: {V, IV, ii},
-            vii: {I}
+            I: {iii, IV, IV6, V, V6, vi, viio},
+            I6: {IV, V, ii, V6, IV6, V64},
+            ii: {IV, V, V6, viio6},
+            ii6: {V, V6, V64, viio6},
+            iii: {IV, vi},
+            iii6: {IV, vi},
+            IV: {V, V6, ii, I, I6},
+            IV6: {V, I, V6, I6},
+            V: {I, vi, viio, I6},
+            V6: {I, I6, vi},
+            V64: {V},
+            vi: {V, IV, ii, I},
+            vi6: {ii, IV, V, I},
+            viio: {I, I6},
+            viio6: {I, I6}
         }
--- src/test_harmony_graph.py
+++ src/test_harmony_graph.py
@@ -3,5 +3,14 @@ from harmony_graph import *
 
 class TestHarmonyGraph(unittest.TestCase):
     def test_access_keys(self):
-        numeral_list = Harmony_Graph().graph[I]
-        self.assertEqual(V in numeral_list, True)
+        hg = Harmony_Graph()
+        numeral_set = hg.graph[I]
+        self.assertEqual(V in numeral_set, True)
+        numeral_set = hg.graph[ii]
+        self.assertEqual(V in numeral_set, True)
+        self.assertEqual(vi in hg.graph[iii], True)
+        self.assertEqual(V in hg.graph[IV], True)
+        self.assertEqual(I in hg.graph[V], True)
+        self.assertEqual(V in hg.graph[vi], True)
+        self.assertEqual(I6 in hg.graph[viio], True)
+
