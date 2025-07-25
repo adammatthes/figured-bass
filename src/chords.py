4a4f6ac74ee3eea6be392e140310bd9970523135
adammatthes <adammatthes@gmail.com>
2025-07-25 15:07:57 -0400

    Add start of chords.py

---
+++ src/chords.py
@@ -0,0 +1,72 @@
+from enum import Enum
+
+from romannumeral import Mode
+
+class Natural(Enum):
+    A = 'a'
+    B = 'b'
+    C = 'c'
+    D = 'd'
+    E = 'e'
+    F = 'f'
+    G = 'g'
+
+
+class Flat(Enum):
+    A = 'ab'
+    B = 'bb'
+    C = 'cb'
+    D = 'db'
+    E = 'eb'
+    F = 'fb'
+    G = 'gb'
+
+
+class Sharp(Enum):
+    A = 'a#'
+    B = 'b#'
+    C = 'c#'
+    D = 'd#'
+    E = 'e#'
+    F = 'f#'
+    G = 'g#'
+
+
+class Chord():
+    
+
+    def __init__(self, roman_numeral, tonic=Natural.C):
+        self.key = self.get_key(roman_numeral, tonic)
+
+
+    def get_key(self, roman_numeral, tonic):
+        order_sharps = ['f', 'c', 'g', 'd', 'a', 'e', 'b']
+        order_flats = reversed(order_sharps)
+
+        # keys with quantities marked 'None' contain double sharps or double flats and will not be supported
+        accidental_quantity = {
+                'c': {'major': {'sharp': 0, 'flat': 0}, 'minor': {'sharp': 0, 'flat': 3}},
+                'd': {'major': {'sharp': 2, 'flat': 0}, 'minor': {'sharp': 0, 'flat': 1}},
+                'e': {'major': {'sharp': 4, 'flat': 0}, 'minor': {'sharp': 1, 'flat': 0}},
+                'f': {'major': {'sharp': 0, 'flat': 1}, 'minor': {'sharp': 0, 'flat': 4}},
+                'g': {'major': {'sharp': 1, 'flat': 0}, 'minor': {'sharp': 0, 'flat': 2}},
+                'a': {'major': {'sharp': 3, 'flat': 0}, 'minor': {'sharp': 0, 'flat': 0}},
+                'b': {'major': {'sharp': 5, 'flat': 0}, 'minor': {'sharp': 2, 'flat': 0}},
+
+                'c#': {'major': {'sharp': 7, 'flat': 0}, 'minor': {'sharp': 4, 'flat': 0}},
+                'd#': {'major': None, 'minor': {'sharp': 6, 'flat': 0}},
+                'e#': {'major': None, 'minor': None}
+                'f#': {'major': {'sharp': 6, 'flat': 0}, 'minor': {'sharp': 3, 'flat': 0}},
+                'g#': {'major': None, 'minor': {'sharp': 5, 'flat', 0}},
+                'a#': {'major': None, 'minor': None}
+                'b#': {'major': None, 'minor': None}
+
+                'cb': {'major': {'sharp': 0, 'flat': 7}, 'minor': None},
+                'db': {'major': {'sharp': 0, 'flat': 5}, 'minor': None},
+                'eb': {'major': {'sharp': 0, 'flat': 3}, 'minor': {'sharp': 0, 'flat': 6}},
+                'fb': {'major': None, 'minor': None},
+                'gb': {'major': {'sharp': 0, 'flat': 6}, 'minor': None},
+                'ab': {'major': {'sharp': 0, 'flat': 4}, 'minor': {'sharp': 0, 'flat': 7}},
+                'bb': {'major': {'sharp': 0, 'flat': 2}, 'minor': {'sharp': 0, 'flat': 5}}
+                }
+
